779f20d4bc04becf7ef31969d5cd8b9ccee8e4c2
Kevin Reece <kevinjreece@gmail.com>
2026-04-28 20:07:59 -0600

    Revert "Add medium-zoom image zoom functionality"
    This reverts commit 27e4db48122b7a98d6c7bc60f5c55afdcf33a071.

--- quartz/components/scripts/imagezoom.inline.ts
+++
@@ -1,9 +0,0 @@
-let zoom: any = null
-
-document.addEventListener("nav", () => {
-  if (zoom) zoom.detach()
-  const mz = (window as any).mediumZoom
-  if (mz) {
-    zoom = mz("article img", { background: "var(--light)" })
-  }
-})
